1ae0c92c4a680c1b85b1a6be41df7d3cff4916b3
Jonathan Janzen <janzej2@mcmaster.ca>
2021-04-05 19:27:56 -0400

    Shop is fully functional, as is the character selection

--- Overdrive/Assets/Scripts/Shop.cs
+++ Overdrive/Assets/Scripts/Shop.cs
@@ -3,14 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using static ShopItems;
+using static Preview;
 
 public class Shop : MonoBehaviour
 {
+    public Dropdown shopDropdown;
+
+    public Text odCoinText;
+
+    private List<CosmeticItem> availableCosmeticsList;
+
     // Start is called before the first frame update
     void Start()
     {
+        if (ShopItems.getPurchasedCosmetics() == null)
+        {
+            ShopItems.setUp();
+        }
 
-        
+        //populate purchased and not-purchased characters into dropdown selection menu
+        availableCosmeticsList = ShopItems.getAllCosmetics();
+        List<string> options = new List<string>();
+        for (int i = 0; i < availableCosmeticsList.Count; i++) {
+            options.Add(availableCosmeticsList[i].getName());
+        }
+        shopDropdown.ClearOptions();
+        shopDropdown.AddOptions(options);
+
+        //get current available OD Coins
+        odCoinText.text = "OD Coins: " + PlayerPrefs.GetFloat("availableOdCoins");
     }
 
     // Update is called once per frame
@@ -24,7 +46,9 @@ public class Shop : MonoBehaviour
         SceneManager.LoadScene("Menu");
     }
 
-    public List<CosmeticItem> getPurchasedCosmetics(){
-        return getPurchasedCosmetics();
+    public void ToPreview() {
+        int previewedItem = shopDropdown.value;
+        Preview.setPreviewCosmetic(availableCosmeticsList[previewedItem]);
+        SceneManager.LoadScene("Preview");
     }
 }
